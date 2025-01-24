4d28b38d76756fe8921a791671aa0ac5bfc0a64e
Al Cutter <al.cutter@gmail.com>
2025-01-24 19:12:00 +0000

    Update bastion feeder to meet C2SP spec

--- internal/feeder/bastion/bastion_feeder_test.go
+++ internal/feeder/bastion/bastion_feeder_test.go
@@ -16,15 +16,30 @@ package bastion
 
 import (
 	"bytes"
+	"context"
 	"encoding/base64"
+	"fmt"
+	"net/http"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
+	"github.com/transparency-dev/formats/note"
+	"github.com/transparency-dev/witness/internal/config"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 const (
-	testCP = "56\n7azctENRYLlBCBQ5OX2qxxIKCTOeCda1KfTwjdt0wdA=\n\n— transparency.dev-aw-ftlog-ci-2 93xidocoWXVph2jEuzW2oovU+IjU71+FeVGKtKXQknSla2HCvr6RYHRSdJfxpo4kj5geqxkjrDXcbpiSo7lK96X4Dgc=\n"
+	testCPOrigin = "transparency.dev/armored-witness/firmware_transparency/ci/2"
+	testCPSize   = 56
+	testCPRoot   = "7azctENRYLlBCBQ5OX2qxxIKCTOeCda1KfTwjdt0wdA="
+	testCPSig    = "— transparency.dev-aw-ftlog-ci-2 93xidocoWXVph2jEuzW2oovU+IjU71+FeVGKtKXQknSla2HCvr6RYHRSdJfxpo4kj5geqxkjrDXcbpiSo7lK96X4Dgc=\n"
+
+	//testCP         = "transparency.dev/armored-witness/firmware_transparency/ci/2\n56\n7azctENRYLlBCBQ5OX2qxxIKCTOeCda1KfTwjdt0wdA=\n\n— transparency.dev-aw-ftlog-ci-2 93xidocoWXVph2jEuzW2oovU+IjU71+FeVGKtKXQknSla2HCvr6RYHRSdJfxpo4kj5geqxkjrDXcbpiSo7lK96X4Dgc=\n"
+	testCPVerifier = "transparency.dev-aw-ftlog-ci-2+f77c6276+AZXqiaARpwF4MoNOxx46kuiIRjrML0PDTm+c7BLaAMt6"
 )
+
+var testCP = fmt.Sprintf("%s\n%d\n%s\n\n%s", testCPOrigin, testCPSize, testCPRoot, testCPSig)
 
 func TestParseBody(t *testing.T) {
 	for _, test := range []struct {
@@ -73,6 +88,108 @@ func TestParseBody(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestHandler(t *testing.T) {
+	v, err := note.NewVerifier(testCPVerifier)
+	if err != nil {
+		t.Fatalf("NewVerifier: %v", err)
+	}
+	logID := "logID"
+	logs := map[string]config.Log{
+		logID: config.Log{Origin: testCPOrigin},
+	}
+	for _, test := range []struct {
+		// params
+		name    string
+		logID   string
+		oldSize uint64
+		// fake witness control
+		witnessResp []byte
+		witnessErr  error
+		// responses
+		wantBody        string
+		wantStatus      int
+		wantContentType string
+	}{
+		{
+			name:        "works - accepted by witness",
+			logID:       "logID",
+			witnessResp: []byte(testCP),
+			wantStatus:  200,
+			wantBody:    testCPSig,
+		}, {
+			name:            "new CP smaller than existing",
+			logID:           "logID",
+			witnessResp:     []byte(testCP),
+			witnessErr:      status.Errorf(codes.AlreadyExists, "test error"),
+			wantStatus:      http.StatusConflict,
+			wantContentType: "text/x.tlog.size",
+			wantBody:        fmt.Sprintf("%d\n", testCPSize),
+		}, {
+			name:        "invalid proof",
+			logID:       "logID",
+			oldSize:     testCPSize,
+			witnessResp: []byte(testCP),
+			witnessErr:  status.Errorf(codes.Unauthenticated, "test error"),
+			wantStatus:  http.StatusUnprocessableEntity,
+		}, {
+			name:            "incorrect oldCP size",
+			logID:           "logID",
+			oldSize:         testCPSize - 10,
+			witnessResp:     []byte(testCP),
+			witnessErr:      status.Errorf(codes.Unauthenticated, "test error"),
+			wantStatus:      http.StatusConflict,
+			wantContentType: "text/x.tlog.size",
+			wantBody:        fmt.Sprintf("%d\n", testCPSize),
+		}, {
+			name:        "same size, different roots",
+			logID:       "logID",
+			oldSize:     testCPSize,
+			witnessResp: []byte(testCP),
+			witnessErr:  status.Errorf(codes.FailedPrecondition, "test error"),
+			wantStatus:  http.StatusConflict,
+		},
+	} {
+		t.Run(test.name, func(t *testing.T) {
+			a := addHandler{
+				w:           &testWitness{updateResponse: test.witnessResp, updateErr: test.witnessErr},
+				witVerifier: v,
+				logs:        logs,
+			}
+			if err != nil {
+				t.Fatalf("NewRequest: %v", err)
+			}
+			sc, body, ct, err := a.handleUpdate(context.Background(), test.logID, testCPOrigin, test.oldSize, []byte(testCP), [][]byte{})
+			if err != nil {
+				t.Fatalf("handleUpdate: %v", err)
+			}
+			if got, want := sc, test.wantStatus; got != want {
+				t.Errorf("handleUpdate got status %d, want %d", got, want)
+			}
+			if got, want := ct, test.wantContentType; got != want {
+				t.Errorf("handleUpdate got content type %q, want %q", got, want)
+			}
+			if got, want := string(body), test.wantBody; got != want {
+				t.Errorf("handleUpdate got body %q, %q", got, want)
+			}
+		})
+	}
+}
+
+type testWitness struct {
+	latestCPErr    error
+	latestCP       []byte
+	updateErr      error
+	updateResponse []byte
+}
+
+func (tw *testWitness) GetLatestCheckpoint(ctx context.Context, logID string) ([]byte, error) {
+	return tw.latestCP, tw.latestCPErr
+}
+
+func (tw *testWitness) Update(ctx context.Context, logID string, newCP []byte, proof [][]byte) ([]byte, error) {
+	return tw.updateResponse, tw.updateErr
 }
 
 func d64(t *testing.T, s string) []byte {
