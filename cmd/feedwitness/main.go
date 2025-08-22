2bb204952aebc5cbeb577523d219afb9dde324eb
Al Cutter <al.cutter@gmail.com>
2025-08-22 12:38:59 +0100

    Improve the tool for feeding checkpoints to witnesses (#394)
    Improves the tool for feeding checkpoints to witnesses by:
    - Renaming the tool to feedwitness to make it clear it's not bastion-specific
    - Simplifies the code by re-using the existing witness client
    - Allows the tool to submit to multiple witnesses

---
+++ cmd/feedwitness/main.go
@@ -0,0 +1,160 @@
+// Copyright 2024 Google LLC. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// feedwitness is a tool for submitting checkpoints from known logs
+// to witnesses, either directly or behind bastions.
+//
+// The primary use case for this tool is testing bastion and/or witness setups,
+// but it may also be useful for feeding checkpoints from logs which do not
+// actively participate in witnessing to witnesses.
+package main
+
+import (
+	"context"
+	"crypto/tls"
+	"errors"
+	"flag"
+	"fmt"
+	"net/http"
+	"net/url"
+	"regexp"
+	"strings"
+
+	w_http "github.com/transparency-dev/witness/client/http"
+	"github.com/transparency-dev/witness/internal/config"
+	"github.com/transparency-dev/witness/internal/witness"
+	"github.com/transparency-dev/witness/omniwitness"
+	"golang.org/x/sync/errgroup"
+	"golang.org/x/time/rate"
+	"gopkg.in/yaml.v3"
+	"k8s.io/klog/v2"
+)
+
+func init() {
+	flag.Var(&witnessURL, "witness_url", "Root URL of the witness to submit checkpoints to (either directly, or via a bastion), can be specified multple times to submit to multiple witnesses")
+}
+
+var (
+	witnessURL    multiStringFlag
+	httpsInsecure = flag.Bool("https_insecure", false, "Set to true to disable TLS verification of the witness service")
+	feed          = flag.String("feed", ".*", "RegEx matching log origins to feed checkpoints from")
+	loopInterval  = flag.Duration("loop_interval", 0, "If set to > 0, runs in looping mode sleeping this duration between feed attempts")
+	rateLimit     = flag.Float64("max_qps", 2, "Defines maximum number of requests/s to send")
+)
+
+type logFeeder struct {
+	cfg  config.Log
+	info omniwitness.LogInfo
+}
+
+func main() {
+	klog.InitFlags(nil)
+	flag.Parse()
+	defer klog.Flush()
+
+	ctx := context.Background()
+	rl := rate.NewLimiter(rate.Limit(*rateLimit), 1)
+
+	httpClient := http.DefaultClient
+	if *httpsInsecure {
+		httpClient = &http.Client{Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+		},
+		}
+	}
+	cfg := omniwitness.LogConfig{}
+	if err := yaml.Unmarshal(omniwitness.ConfigLogs, &cfg); err != nil {
+		klog.Exitf("failed to unmarshal witness config: %v", err)
+	}
+
+	feeders := make(map[string]logFeeder)
+	for _, l := range cfg.Logs {
+		lc, err := config.NewLog(l.Origin, l.PublicKey, l.URL)
+		if err != nil {
+			klog.Exitf("invalid log configuration: %v", err)
+		}
+		if l.Feeder != omniwitness.None {
+			feeders[l.Origin] = logFeeder{
+				cfg:  lc,
+				info: l,
+			}
+		}
+	}
+
+	r := regexp.MustCompile(*feed)
+	if len(witnessURL) == 0 {
+		klog.Exitf("At least one --witness_url must be specifed")
+	}
+	eg := errgroup.Group{}
+	for _, wu := range witnessURL {
+		u, err := url.Parse(wu)
+		if err != nil {
+			klog.Exitf("Invalid witness URL %q: %v", wu, err)
+		}
+		bc := &loggingClient{
+			witness: w_http.NewWitness(u, httpClient),
+			url:     wu,
+		}
+		for o, lf := range feeders {
+			if r.Match([]byte(o)) {
+				eg.Go(func() error {
+					if err := rl.Wait(ctx); err != nil {
+						return err
+					}
+					return lf.info.Feeder.FeedFunc()(ctx, lf.cfg, bc.Update, httpClient, *loopInterval)
+				})
+			}
+		}
+	}
+	if err := eg.Wait(); err != nil {
+		klog.Errorf("%v", err)
+	}
+}
+
+type loggingClient struct {
+	witness w_http.Witness
+	url     string
+}
+
+// Update attempts to clock the witness forward for the given logID.
+// The latest signed checkpoint will be returned if this succeeds, or if the error is
+// http.ErrCheckpointTooOld. In all other cases no checkpoint should be expected.
+func (lc *loggingClient) Update(ctx context.Context, oldSize uint64, newCP []byte, proof [][]byte) ([]byte, uint64, error) {
+	rb, size, err := lc.witness.Update(ctx, oldSize, newCP, proof)
+
+	switch name := strings.Split(string(newCP), "\n")[0]; {
+	case err == nil:
+		klog.Infof("✅ %s ← %s: updated with signature(s):\n%s", lc.url, name, string(rb))
+	case errors.Is(err, witness.ErrCheckpointStale):
+		msg := fmt.Sprintf("View stale, witness has checkpoint size %d (we have %d)", size, oldSize)
+		klog.Infof("🫣 %s ← %s: conflict: %s", lc.url, name, msg)
+	default:
+		klog.Infof("❌ %s ← %s: %v", lc.url, name, err)
+	}
+
+	return nil, 0, nil
+}
+
+// multiStringFlag allows a flag to be specified multiple times on the command
+// line, and stores all of these values.
+type multiStringFlag []string
+
+func (ms *multiStringFlag) String() string {
+	return strings.Join(*ms, ",")
+}
+
+func (ms *multiStringFlag) Set(w string) error {
+	*ms = append(*ms, w)
+	return nil
+}
